99d5c06f8f35bee68f4336a765361adc8a0c3686
U-LENOVO-PC\zhengquan <zhengquan.hu@qq.com>
2016-01-31 18:07:48 +0800

    微信墙

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ app.keys = ['kindle','lumia','meet'];
 app.use(require('koa-logger')());
 
 //商店
-app.use(mount("/shop",require('./shop').app));
+//app.use(mount("/shop",require('./shop').app));
 
 //微信功能
-app.use(mount("/weixin",require("./weixin").app));
+//app.use(mount("/weixin",require("./weixin").app));
 
-app.listen(process.env.PORT || 3000);
+//墙
+var wall=require("./wall");
+app.use(mount("/wall",wall.app));
+
+var server = require('http').Server(app.callback());
+wall.initialize(server);
+
+server.listen(process.env.PORT || 3000);
 //app.listen(process.env.PORT || 3000);
 console.log('listening on port 3000');
 
